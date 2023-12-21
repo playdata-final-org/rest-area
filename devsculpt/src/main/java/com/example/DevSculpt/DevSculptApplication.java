5dd887a4999538d161835a2738da2b8dd7967443
Kim se young <jack9601@naver.com>
2023-12-21 16:22:00 +0900

    [chore] devsculpt 프로젝트 구조 셋팅
    코드베이스 통합

---
+++ devsculpt/src/main/java/com/example/DevSculpt/DevSculptApplication.java
@@ -0,0 +1,12 @@
+package com.example.DevSculpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSculptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSculptApplication.class, args);
+	}
+}

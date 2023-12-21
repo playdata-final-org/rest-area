c982aba0af84a5cb14788c9fe64a7df1128183eb
Kim se young <jack9601@naver.com>
2023-12-21 17:52:43 +0900

    [chore] devsculpt 프로젝트 기본 셋팅

---
+++ devsculpt/src/main/java/com/example/DevSculpt/DevSculptApplication.java
@@ -0,0 +1,13 @@
+package com.example.DevSculpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSculptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSculptApplication.class, args);
+	}
+
+}
---
+++ devsculpt/src/test/java/com/example/DevSculpt/DevSculptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DevSculpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSculptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

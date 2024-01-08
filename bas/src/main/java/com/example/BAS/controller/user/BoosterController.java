0acc2c739daf3a99b5d758706a949be7a2ed9275
ErimMid <rururia38@gmail.com>
2024-01-08 09:16:42 +0900

    [feat] 블로그 프론트 컨트롤러 매핑
    프론트 연결을 위한 매핑 작업

--- bas/src/main/java/com/example/BAS/controller/user/BoosterController.java
+++ bas/src/main/java/com/example/BAS/controller/user/BoosterController.java
@@ -2,54 +2,14 @@ package com.example.BAS.controller.user;
 
 import com.example.BAS.config.security.PrincipalDetails;
 import com.example.BAS.entitiy.users.Users;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
-@RequiredArgsConstructor
-@Slf4j
-public class BlogController {
-
-    @GetMapping("/blogHome")
-    public String showBlogHome() {
-        return "blog/blog-home";
-    }
-    @GetMapping("/blogCollection")
-    public String showBlogCollection() {
-        return "blog/blog-collection";
-    }
-    @GetMapping("/blogMembership")
-    public String showMembership() {
-        return "blog/blog-membership";
-    }
-    @GetMapping("/blogAbout")
-    public String showBlogAbout() {
-        return "blog/blog-about";
-    }
-
-    @GetMapping("/blogWrite")
-    public String showBlogWrite(@AuthenticationPrincipal PrincipalDetails principalDetails, Model model) {
-        if ("ROLE_BOOSTER".equalsIgnoreCase(principalDetails.getUsers().getAuthority().getAuthorityName())) {
-            Users booster = principalDetails.getUsers();
-            if (booster.getProfileImage() != null) {
-                // UUID와 파일 이름을 모델에 추가
-                model.addAttribute("profileImageUuid", booster.getProfileImage().getUuid());
-                model.addAttribute("profileImageFileName", booster.getProfileImage().getFileName());
-            }
-            model.addAttribute("booster", booster);
-            System.out.println("booster = " + booster);
-            return "blog/blog-write";
-        } else {
-            return "user/main";
-        }
-    }
-
-    @GetMapping("/blogMembershipWrite")
+public class BoosterController {
+    @GetMapping("/creator-search")
     public String showBlogMembershipWrite(@AuthenticationPrincipal PrincipalDetails principalDetails, Model model) {
         if ("ROLE_BOOSTER".equalsIgnoreCase(principalDetails.getUsers().getAuthority().getAuthorityName())) {
             Users booster = principalDetails.getUsers();
@@ -60,14 +20,14 @@ public class BlogController {
             }
             model.addAttribute("booster", booster);
             System.out.println("booster = " + booster);
-            return "blog/blog-write-membershipChoose";
+            return "user/creator-search";
         } else {
             return "user/main";
         }
     }
 
-    @GetMapping("/aboutWrite")
-    public String showBlogAboutWrite(@AuthenticationPrincipal PrincipalDetails principalDetails, Model model) {
+    @GetMapping("/creatorSearchList")
+    public String showCreatorSearchList(@AuthenticationPrincipal PrincipalDetails principalDetails, Model model) {
         if ("ROLE_BOOSTER".equalsIgnoreCase(principalDetails.getUsers().getAuthority().getAuthorityName())) {
             Users booster = principalDetails.getUsers();
             if (booster.getProfileImage() != null) {
@@ -77,7 +37,7 @@ public class BlogController {
             }
             model.addAttribute("booster", booster);
             System.out.println("booster = " + booster);
-            return "blog/blog-aboutWrite";
+            return "user/creator-search-list";
         } else {
             return "user/main";
         }

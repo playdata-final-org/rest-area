731e06ad36cd680bd1e7913d049cb6008b966095
전영빈 <kasdf12347@naver.com>
2024-01-16 17:04:26 +0900

    [feat] mail front end 1차 작업

---
+++ rest-mail/src/main/java/com/restarea/mail/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.restarea.mail.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/asset/**")
+                        .addResourceLocations("classpath:/static/asset/");
+        registry.addResourceHandler("/dist/**")
+                .addResourceLocations("classpath:/static/dist/");
+
+    }
+
+
+}
---
+++ rest-mail/src/main/resources/static/src/js/summernote.js
@@ -0,0 +1,14 @@
+$("#summernote").summernote({
+  placeholder: "메일을 작성해 주세요",
+  tabsize: 2,
+  height: 120,
+  toolbar: [
+    ["style", ["style"]],
+    ["font", ["bold", "underline", "clear"]],
+    ["color", ["color"]],
+    ["para", ["ul", "ol", "paragraph"]],
+    ["table", ["table"]],
+    ["insert", ["link", "picture", "video"]],
+    ["view", ["fullscreen", "codeview", "help"]],
+  ],
+});
